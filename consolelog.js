c25addebc70d5a419e6d591f07713faa9a4cf715
Rafael Mocellin <rafael.mocellin.leszczynski@gmail.com>
2022-07-11 11:10:01 -0300

    first commit

---
+++ consolelog.js
@@ -0,0 +1,47 @@
+let nome = 'Rafael';
+let sobrenome = 'Mocellin';
+let segundosobrenome = 'Leszczynski';
+let idade = 20;
+let frase = 'Olá meu nome é ' + nome + ', eu tenho ' + idade + ' anos de idade...!';
+
+let frase2 ='Este é o Console.log do meu projeto de formulário, criado para ser visualizado somente pelo console, utilizei variáveis Let no JavaScript para acrescentar todas essas informações.'
+
+console.log(frase, frase2);
+
+let explicacao = {
+    explicação:'Aqui abaixo algumas informações minhas...'
+    
+}
+console.log(explicacao)
+
+let info = {
+    nome: 'Rafael',
+    sobrenomes: 'Mocellin Leszczynski',
+    idade: '20 anos',
+   
+};
+console.log(info)
+
+
+
+let novasInfo = {
+    
+    cidade: 'Porto Alegre',
+    estado: 'Rio Grande do Sul',
+    pais: 'Brasil',
+};
+
+console.log(novasInfo)
+
+let redes = {
+    facebook: '/rafael.mocellin.52',
+    github: '/RafaelMocellinL19',
+    instagram:'/r4f4leszczynsk1/'
+}
+console.log(redes)
+
+let outrasRedes = {
+    linkedIn: 'linkedin.com/in/rafaelmocellin/',
+    whatsapp:'https://wa.me/5551989533261'
+}
+console.log(outrasRedes)
